954adfa83a1b009c73f03a3261ad442c3b2eb8dc
gitee <491919843@qq.com>
2022-09-25 00:03:34 +0800

    test

--- api/getToken.js
+++ api/getToken.js
@@ -2,6 +2,6 @@ export default function handler(request, response) {
     // const { name } = request.query;
     console.log(request)
     // console.log(response)
-    response.status(200).send(`param: ${request}`);
+    response.status(200).send(`param: ${ JSON.stringify(request)}`);
   }
   
